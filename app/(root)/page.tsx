76b344f84f99261cb4dac1f8db23b916b4738c5c
B-Shankar <learn.bhimashankar@gmail.com>
2025-10-24 00:16:36 +0530

    Fix the widget configuration mismatch

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
                     <TradingViewWidget
                         title="Market Overview"
                         scriptUrl={`${scriptUrl}market-overview.js`}
-                        config={HEATMAP_WIDGET_CONFIG}
+                        config={MARKET_OVERVIEW_WIDGET_CONFIG}
                         className="custom-chart"
                         height={600}
                     />
