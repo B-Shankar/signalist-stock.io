f753425539be5837d555fc194d8bf171a1155291
B-Shankar <learn.bhimashankar@gmail.com>
2025-10-31 00:19:13 +0530

    removed static image for profile

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -33,7 +33,7 @@ const UserDropdown = ({ user, initialStocks } : { user : User, initialStocks : S
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="https://avatars.githubusercontent.com/u/149028707?s=96&v=4" />
+                        {/*<AvatarImage src="https://avatars.githubusercontent.com/u/149028707?s=96&v=4" />*/}
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                             {(user.name?.[0] ?? '?').toUpperCase()}
                         </AvatarFallback>
@@ -49,7 +49,7 @@ const UserDropdown = ({ user, initialStocks } : { user : User, initialStocks : S
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
-                            <AvatarImage src="https://avatars.githubusercontent.com/u/149028707?s=96&v=4" />
+                            {/*<AvatarImage src="https://avatars.githubusercontent.com/u/149028707?s=96&v=4" />*/}
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                                 {user.name[0]}
                             </AvatarFallback>
